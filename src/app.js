ebbc1ac05533ce9a8d81da64f94b192819fd9d65
tushar <tushar.acharrya@gmail.com>
2024-09-20 18:31:33 +0530

    installed postman

--- src/app.js
+++ src/app.js
@@ -2,21 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.use("/",(req,res)=>{                                // this function is known as request handler
-    res.send("kya re perpendicular kaisa hai");     // will only respond to /test
-
+app.use("/hello/2",(req,res)=>{
+    res.send("hello2");
 })
 
-
+app.use("/hello",(req,res)=>{
+    res.send("hello");
+})
 
 app.use("/test",(req,res)=>{                                // this function is known as request handler
     res.send("hello world from the server side");     // will only respond to /test
 
 })
+app.use("/",(req,res)=>{
+    res.send("kya re lund fakir");
+})
 
 
 
-app.listen(3009,()=>{
-    console.log(" server is running on port 3009");
+app.listen(7000,()=>{
+    console.log(" server is running on port 7000");
 
 });
