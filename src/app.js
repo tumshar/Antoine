2d7ce17338be9943f1431ad620f6350c912f7115
tushar <tushar.acharrya@gmail.com>
2024-09-20 19:28:11 +0530

    advanced routing techniques

--- src/app.js
+++ src/app.js
@@ -2,42 +2,17 @@ const express = require('express');
 
 const app = express();
 
-//app.use("/hello/2",(req,res)=>{
-  //  res.send("hello2");
-//})
 
-//app.use("/hello",(req,res)=>{
-  //  res.send("hello");
-//})
-app.get("/user",(req, res)=>{
+app.get("/user/:userid/:name/:password",(req, res)=>{    
+  console.log(req.params);                 // ac and abc both will be handled
     res.send({firstname:"tushar", age:"22"})
-})  // this will only handle get call to .../user
-
-
-app.post("/user",(req, res)=>{
-    console.log("save data to the database");
-    res.send("data saved to the database"); // saving data to the database
-})
-
-
-
-app.delete("/user",(req, res)=>{
-    console.log("deleted succesfully");
-    res.send("delete data"); // saving data to the database
-})
+})  
 
 
 
 
 
 
-app.use("/test",(req,res)=>{                                // this function is known as request handler
-    res.send("hello world from the server side");     // will only respond to /test
-
-})
-//app.use("/",(req,res)=>{
-  //  res.send("kya re lund fakir");
-//})
 
 
 
